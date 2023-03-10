3d1892bf53a23e1f4552952484bcba6c902b5555
Felipe da Hora <lipedahora2000@hotmail.com>
2023-03-10 20:28:02 -0300

    Aula 04.01

--- bin/aula02.dart
+++ bin/aula02.dart
@@ -1,22 +1,39 @@
 import '../lib/Aula 04/Classes/aluno.dart';
 import '../lib/Aula 04/Classes/professor.dart';
+import '../lib/Aula 04/Classes/alunoComFuncao.dart';
+
+
+void minhaFuncao(){
+  print("buscar livro");
+}
 
 void main(List<String> arguments) {
-  var aluno = Aluno.construtorNomimal(RA: 123456789, nome: "Felipe Adrian da Hora de Arruda", CPF: "000.000.000-00", nascimento:DateTime.now());
-  
-  var aluno2 = Aluno.construtorPadrao(
-      12345678910,
-      "Felipe Adrian da Hora de Arruda 2",
-      "000.000.000-10",
-      DateTime.now());
+  var aluno = Aluno.construtorNomimal(
+      RA: 123456789,
+      nome: "Felipe Adrian da Hora de Arruda",
+      CPF: "000.000.000-00",
+      nascimento: DateTime.now());
+
+  var aluno2 = Aluno.construtorPadrao(12345678910,
+      "Felipe Adrian da Hora de Arruda 2", "000.000.000-10", DateTime.now());
+
+  var alunoComFuncao = AlunoComFuncao(
+      RA: 123456789,
+      nome: "Felipe Adrian da Hora de Arruda",
+      CPF: "000.000.000-00",
+      nascimento: DateTime.now(),
+      minhaFuncao: minhaFuncao );
 
   print("${aluno.nome} \n ${aluno.CPF} \n ${aluno.RA} \n ${aluno.nascimento}");
   print("\n");
 
-  print("${aluno2.nome} \n ${aluno2.CPF} \n ${aluno2.RA} \n ${aluno2.nascimento}");
+  print(
+      "${aluno2.nome} \n ${aluno2.CPF} \n ${aluno2.RA} \n ${aluno2.nascimento}");
   print("\n");
 
-  var professor = Professor(987654321, "Helio Kamakawa", "111.111.111-11", DateTime.now(), DateTime.now(), "heliokamakawa@ifpr.com.br");
+  var professor = Professor(987654321, "Helio Kamakawa", "111.111.111-11",
+      DateTime.now(), DateTime.now(), "heliokamakawa@ifpr.com.br");
 
-  print("${professor.nome} \n ${professor.CPF} \n ${professor.SIAPE} \n ${professor.dataEntrada} \n ${professor.email}");
+  print(
+      "${professor.nome} \n ${professor.CPF} \n ${professor.SIAPE} \n ${professor.dataEntrada} \n ${professor.email}");
 }
